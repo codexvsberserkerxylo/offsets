7bc1fd0a09018edcc9ac0763b922757df0d3d953
nick <septembereleventwoten@gmail.com>
2026-05-13 03:00:15 +0200

    Create encryptions.hpp

---
+++ encryptions.hpp
@@ -0,0 +1,17 @@
+//NOT MINE.
+//current luau: https://github.com/luau-lang/luau
+
+#define PROTO_LINEINFO_ENC     vmval1
+#define PROTO_LOCVARS_ENC      vmval1
+#define PROTO_UPVALUES_ENC     vmval4
+#define PROTO_DEBUGINSN_ENC    vmval1
+#define PROTO_TYPEINFO_ENC     vmval4
+#define PROTO_SOURCE_ENC       vmval3
+#define PROTO_ABSLINEINFO_ENC  vmval1
+#define PROTO_DEBUGNAME_ENC    vmval4
+#define PROTO_USERDATA_ENC     vmval3
+#define UDATA_META_ENC         vmval1
+#define CLOSURE_DEBUGNAME_ENC  vmval2
+#define CLOSURE_CONT_ENC       vmval1
+#define TSTRING_HASH_ENC       vmval1
+#define LSTATE_STACKSIZE_ENC   vmval1

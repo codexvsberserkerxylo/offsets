0ee40394721561ace223ab372d71b913ad212179
nick <septembereleventwoten@gmail.com>
2026-05-13 02:33:49 +0200

    Add files via upload

---
+++ version-bf6344c9c23446bf/encryptions.hpp
@@ -0,0 +1,18 @@
+// NOT mine
+
+#pragma once
+
+#define PROTO_USERDATA_ENC        LuaVMValue3
+#define PROTO_LOCVARS_ENC         LuaVMValue1
+#define PROTO_DEBUGINSN_ENC       LuaVMValue1
+#define PROTO_TYPEINFO_ENC        LuaVMValue4
+#define PROTO_UPVALUES_ENC        LuaVMValue1
+#define PROTO_LINEINFO_ENC        LuaVMValue1
+#define PROTO_SOURCE_ENC          LuaVMValue3
+#define PROTO_DEBUGNAME_ENC       LuaVMValue4
+#define PROTO_ABSLINEINFO_ENC     LuaVMValue1
+#define LSTATE_STACKSIZE_ENC      LuaVMValue1
+#define TSTRING_HASH_ENC          LuaVMValue1
+#define UDATA_METATABLE_ENC       LuaVMValue1
+#define CLOSURE_DEBUGNAME_ENC     LuaVMValue2
+#define CLOSURE_CONT_ENC          LuaVMValue1

1ff2e0e7bd7d560cd0c778885c027204c3c639f6
nick <septembereleventwoten@gmail.com>
2026-05-18 13:15:31 +0200

    Add task library constants to strings.hpp
    Added new task library constants and documentation for task functions.

--- strings.hpp
+++ strings.hpp
@@ -15,3 +15,38 @@ const uintptr_t OpcodeLookupTable = ; R%d: %s%s from %d to %d
 const uintptr_t FireTouchInterest = new overlap in different world 
 const uintptr_t GetGlobalState = Script Start
 const uintptr_t GetFFlag = _DataCenterFilter
+
+// some new ones
+const uintptr_t Print = Breakpoint %s:%d ignored: %s
+
+// task lib
+const uintptr_t task.defer = task.defer is not available for AuroraScripts
+const uintptr_t task.spawn = task.spawn is not available for AuroraScripts
+const uintptr_t task.desynchronize = task.desynchronize() may only be called from a script that is a descendant of an Actor
+const uintptr_t task.synchronize = task.synchronize() may only be called from a script that is a descendant of an Actor
+const uintptr_t task.wait = task.wait is not available for AuroraScripts
+
+// other method for task lib (wayy better tbh)
+/*
+ * showcased version: version-ec412128eba3476e
+ * 
+ * how to find them:
+ * search for the string "desynchronize" and check its xrefs
+ * you will find the registration block for the task functions:
+ *
+ *   qword_761AD80 = (__int64)"synchronize";
+ *   qword_761AD88 = (__int64)sub_1DAB5F0;   // task.synchronize
+ *   qword_761AD90 = (__int64)"desynchronize";
+ *   qword_761AD98 = (__int64)sub_1DAABC0;   // task.desynchronize
+ *   qword_761ADA0 = (__int64)"defer";
+ *   qword_761ADA8 = (__int64)qword_1DAA520; // task.defer
+ *   qword_761ADB0 = (__int64)"spawn";
+ *   qword_761ADB8 = (__int64)sub_1DAB470;   // task.spawn
+ *   qword_761ADC0 = (__int64)"delay";
+ *   qword_761ADC8 = (__int64)sub_1DAA980;   // task.delay
+ *   qword_761ADD0 = (__int64)"wait";
+ *   qword_761ADD8 = (__int64)qword_1DAB800; // task.wait
+ *   qword_761ADE0 = (__int64)"cancel";
+ *   qword_761ADE8 = (__int64)sub_1DAA210;   // task.cancel
+ * and these are all offsets for the task lib ez im so tuff (found ts by myself accidentally
+ */

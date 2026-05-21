f6601ee21c5d2a332a5b992ecca7536c732bbf0b
nick <septembereleventwoten@gmail.com>
2026-05-22 01:47:29 +0200

    updated to version-4b6315bf1f0a4dbb

--- hyperion.hpp
+++ hyperion.hpp
@@ -1,20 +1,20 @@
 // dumped by nick (hypdumper) \\
-// date: 2026-05-20 01:36:38 \\
-// took 0.309s \\
+// date: 2026-05-22 01:46:14 \\
+// took 0.308s \\
 
 #pragma once
 #include <cstdint>
 
 namespace Hyperion
 {
-    static constexpr uintptr_t ControlFlowGuard = 0x1482210;
-    static constexpr uintptr_t BitMap = 0x1567be8;
+    static constexpr uintptr_t ControlFlowGuard = 0xea80e0;
+    static constexpr uintptr_t BitMap = 0x157c368;
 
     enum Offsets    {
-        ByteShift = 15,   // 0xf
-        PageShift = 12,   // 0xc
-        BitMask   = 7,   // 0x7
-        PageSize  = 0x1000,  // 4096
-        PageMask  = 0xfff   // 4095
+        ByteShift = 15, // 0xf
+        PageShift = 12, // 0xc
+        BitMask   = 7, // 0x7
+        PageSize  = 0x1000, // 4096
+        PageMask  = 0xfff // 4095
     };
 } // namespace Hyperion

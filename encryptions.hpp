514bc5e4c74132c6afa69c2d953cff33c8be4c1a
nick <septembereleventwoten@gmail.com>
2026-05-28 16:49:28 +0200

    Create encryptions.hpp

---
+++ encryptions.hpp
@@ -0,0 +1,18 @@
+#define CLOSURE_DEBUGNAME_ENC VMValue2
+#define CLOSURE_CONT_ENC      VMValue4
+
+#define LSTATE_STACKSIZE_ENC VMValue1
+
+#define PROTO_DEBUGINSN_ENC   VMValue4
+#define PROTO_TYPEINFO_ENC    VMValue1
+#define PROTO_UPVALUES_ENC    VMValue4
+#define PROTO_SOURCE_ENC      VMValue2
+#define PROTO_ABSLINEINFO_ENC VMValue1
+#define PROTO_USERDATA_ENC    VMValue4
+#define PROTO_DEBUGNAME_ENC   VMValue3
+#define PROTO_LOCVARS_ENC     VMValue3
+#define PROTO_LINEINFO_ENC    VMValue2
+
+#define UDATA_META_ENC       VMValue4
+
+#define TSTRING_HASH_ENC     VMValue4

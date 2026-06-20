96d75ef949766b0f2d2c44a2d13376bfec28726d
nick <septembereleventwoten@gmail.com>
2026-06-20 02:08:51 +0200

    a

--- encryptions.hpp
+++ encryptions.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
-#define PROTO_DEBUGNAME_ENC VMValue2
-#define PROTO_DEBUGINSN_ENC VMValue3
-#define PROTO_UPVALUES_ENC VMValue2
-#define PROTO_ABSLINEINFO_ENC VMValue2
-#define PROTO_USERDATA_ENC VMValue0  // needs-verify
-#define PROTO_LINEINFO_ENC VMValue1
-#define PROTO_LOCVARS_ENC VMValue1
-#define PROTO_TYPEINFO_ENC VMValue4
-#define PROTO_SOURCE_ENC VMValue1
-#define CLOSURE_DEBUGNAME_ENC VMValue2
-#define CLOSURE_CONT_ENC VMValue4
-#define UDATA_META_ENC VMValue4
-#define TSTRING_HASH_ENC VMValue4
-#define LSTATE_STACKSIZE_ENC VMValue1
+#define CLOSURE_CONT_ENC      vmvalue4
+#define CLOSURE_DEBUGNAME_ENC vmvalue2
+
+#define LSTATE_STACKSIZE_ENC  vmvalue1
+
+#define PROTO_UPVALUES_ENC    vmvalue2
+#define PROTO_SOURCE_ENC      vmvalue1
+#define PROTO_USERDATA_ENC    vmvalue3
+#define PROTO_DEBUGNAME_ENC   vmvalue2
+#define PROTO_DEBUGINSN_ENC   vmvalue3
+#define PROTO_TYPEINFO_ENC    vmvalue4
+#define PROTO_LINEINFO_ENC    vmvalue1
+#define PROTO_ABSLINEINFO_ENC vmvalue2
+#define PROTO_LOCVARS_ENC     vmvalue1
+
+#define TSTRING_HASH_ENC      vmvalue4

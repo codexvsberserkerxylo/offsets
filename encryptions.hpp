e6608fe17d865a69957b3273a9e611c600169aee
nick <septembereleventwoten@gmail.com>
2026-07-01 18:38:27 +0200

    Update encryptions.hpp

--- encryptions.hpp
+++ encryptions.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
-#define CLOSURE_CONT_ENC      vmvalue4
-#define CLOSURE_DEBUGNAME_ENC vmvalue2
-
-#define LSTATE_STACKSIZE_ENC  vmvalue1
-
-#define PROTO_UPVALUES_ENC    vmvalue2
-#define PROTO_SOURCE_ENC      vmvalue1
-#define PROTO_USERDATA_ENC    vmvalue3
-#define PROTO_DEBUGNAME_ENC   vmvalue2
-#define PROTO_DEBUGINSN_ENC   vmvalue3
-#define PROTO_TYPEINFO_ENC    vmvalue4
-#define PROTO_LINEINFO_ENC    vmvalue1
-#define PROTO_ABSLINEINFO_ENC vmvalue2
-#define PROTO_LOCVARS_ENC     vmvalue1
-
-#define TSTRING_HASH_ENC      vmvalue4
+#define PROTO_DEBUGNAME_ENC VMValue4
+#define PROTO_DEBUGINSN_ENC VMValue2
+#define PROTO_UPVALUES_ENC VMValue4
+#define PROTO_ABSLINEINFO_ENC VMValue3
+#define PROTO_USERDATA_ENC VMValue1
+#define PROTO_LINEINFO_ENC VMValue2
+#define PROTO_LOCVARS_ENC VMValue3
+#define PROTO_TYPEINFO_ENC VMValue1
+#define PROTO_SOURCE_ENC VMValue3
+#define CLOSURE_DEBUGNAME_ENC VMValue2
+#define CLOSURE_CONT_ENC VMValue4
+#define UDATA_META_ENC VMValue3
+#define TSTRING_HASH_ENC VMValue1
+#define LSTATE_STACKSIZE_ENC VMValue3

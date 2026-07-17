aa132100222baf65b457b2a65adf088fdd9869eb
nick <septembereleventwoten@gmail.com>
2026-07-17 11:42:11 +0200

    Create encryptions.hpp

---
+++ encryptions.hpp
@@ -0,0 +1,17 @@
+// dumped by nick
+// verified in ida
+
+#define CLOSURE_CONT_ENC VMValue4
+#define CLOSURE_DEBUGNAME_ENC VMValue2
+#define LSTATE_STACKSIZE_ENC VMValue3
+#define PROTO_ABSLINEINFO_ENC VMValue3
+#define PROTO_DEBUGINSN_ENC VMValue3
+#define PROTO_DEBUGNAME_ENC VMValue2
+#define PROTO_LINEINFO_ENC VMValue4
+#define PROTO_LOCVARS_ENC VMValue2
+#define PROTO_SOURCE_ENC VMValue4
+#define PROTO_TYPEINFO_ENC VMValue1
+#define PROTO_UPVALUES_ENC VMValue3
+#define PROTO_USERDATA_ENC VMValue4
+#define TSTRING_HASH_ENC VMValue1
+#define UDATA_META_ENC VMValue1

9fb43a37e2cc6f5b9735ffa3108b01709fda2dc3
nick <septembereleventwoten@gmail.com>
2026-08-18 03:37:14 +0200

    Refactor string constants in strings.hpp
    Updated string constants to use proper formatting and added new entries for various functions and error messages.

--- strings.hpp
+++ strings.hpp
@@ -12,37 +12,38 @@
 // 10 stars on this shit and ill add more strings
 
 // some new ones (tested on version-5cf2272675e145f5)
-const uintptr_t print = Breakpoint %s:%d ignored: %s
-const uintptr_t luaL_register = name conflict for module '%s'
-const uintptr_t luaH_dummynode = "{\"type\":\"table\",\"cat\":%d,\"size\":%d" // can use ,\"pairs\":[ and \"%p\" and ,\"metatable\": and more too
-const uintptr_t overlap = new overlap in different world // look for a number, convert it to hex (e.g. if u see 504 and press h its 0x1f8 so thats the overlap offset)
-const uintptr_t Register = Local player already exists
-const uintptr_t RobloxLogCrash = Invalid Facet Access // sub_4AF5610(0, "Invalid Facet Access"); like this
-const uintptr_t coroutine.close = cannot close %s coroutine
-const uintptr_t luaB_newproxy = nil or boolean
-const uintptr_t luaB_pcallrun = xpcall
-const uintptr_t luaB_pcall = pcall
-const uintptr_t luaB_setmetatable = cannot change a protected metatable
-const uintptr_t luaB_setfenv = 'setfenv' cannot change environment of given object
-const uintptr_t luaB_tonumber = base out of range
-const uintptr_t luaB_rawlen = table or string expected
-const uintptr_t luaB_getmetatable = __metatable // func at xref
-const uintptr_t luaL_getmetafield = __metatable // first call in func
-const uintptr_t luaopen_base = xpcall
-const uintptr_t luaB_assert = assertion failed!
-const uintptr_t lua_pushfstringL = %s: bytecode version mismatch (expected [%d..%d], got %d) // call after xref
-const uintptr_t luaV_gettable = '__index' chain too long; possible loop
-const uintptr_t luaV_settable = '__newindex' chain too long; possible loop // theres luaO_nilobject in here btw :v:
-const uintptr_t lua_break = attempt to break across metamethod/C-call boundary
-const uintptr_t lua_yield = attempt to yield across metamethod/C-call boundary // small func is it but bigger func is Instance::WaitForChild
-const uintptr_t luaG_runerrorl = '__index' chain too long; possible loop // call after xref
-const uintptr_t luaopen_math = sqrt2 // func at xre´f
-const uintptr_t lua_setfield = sqrt2 // call after xref
-const uintptr_t luaL_typeerrorL = invalid argument #%d to '%s' (%s expected, got %s)
-const uintptr_t luaL_argerrorl = invalid argument #%d to '%s' (%s)
-const uintptr_t pusherror = error in error handling // func at xref
-const uintptr_t f_luaopen = error in error handling // second func at xref
-const uintptr_t GetFFlag = [FLog::ResetFilters] ParseAndSetFilterValue saving {} = {} for {} // 
+const uintptr_t print = "Breakpoint %s:%d ignored: %s"
+const uintptr_t luaL_register = "name conflict for module '%s'"
+const uintptr_t luaH_dummynode = "{\"type\":\"table\",\"cat\":%d,\"size\":%d" // can use ,\"pairs\":[ and \"%p\" and ,\"metatable\": and more too, its a unk a little over the string i think 
+const uintptr_t overlap = "new overlap in different world" // look for a number, convert it to hex (e.g. if u see 504 and press h its 0x1f8 so thats the overlap offset)
+const uintptr_t Register = "Local player already exists"
+const uintptr_t RobloxLogCrash = "Invalid Facet Access" // sub_4AF5610(0, "Invalid Facet Access"); like this
+const uintptr_t coroutine.close = "cannot close %s coroutine"
+const uintptr_t luaB_newproxy = "nil or boolean"
+const uintptr_t luaB_pcallrun = "xpcall"
+const uintptr_t luaB_pcall = "pcall"
+const uintptr_t luau_load = "bytecode corrupted"
+const uintptr_t luaB_setmetatable = "cannot change a protected metatable"
+const uintptr_t luaB_setfenv = "'setfenv' cannot change environment of given object"
+const uintptr_t luaB_tonumber = "base out of range"
+const uintptr_t luaB_rawlen = "table or string expected"
+const uintptr_t luaB_getmetatable = "__metatable" // func at xref
+const uintptr_t luaL_getmetafield = "__metatable" // first call in func
+const uintptr_t luaopen_base = "xpcall"
+const uintptr_t luaB_assert = "assertion failed!"
+const uintptr_t lua_pushfstringL = "%s: bytecode version mismatch (expected [%d..%d], got %d)" // call after xref
+const uintptr_t luaV_gettable = "'__index' chain too long; possible loop"
+const uintptr_t luaV_settable = "'__newindex' chain too long; possible loop" // theres luaO_nilobject in here btw :v:
+const uintptr_t lua_break = "attempt to break across metamethod/C-call boundary"
+const uintptr_t lua_yield = "attempt to yield across metamethod/C-call boundary" // small func is it but bigger func is Instance::WaitForChild
+const uintptr_t luaG_runerrorl = "'__index' chain too long; possible loop" // call after xref
+const uintptr_t luaopen_math = "sqrt2" // func at xre´f
+const uintptr_t lua_setfield = "sqrt2" // call after xref
+const uintptr_t luaL_typeerrorL = "invalid argument #%d to '%s' (%s expected, got %s)"
+const uintptr_t luaL_argerrorl = "invalid argument #%d to '%s' (%s)"
+const uintptr_t pusherror = "error in error handling" // func at xref
+const uintptr_t f_luaopen = "error in error handling" // second func at xref
+const uintptr_t GetFFlag = "[FLog::ResetFilters] ParseAndSetFilterValue saving {} = {} for {}"
 /*
     if ( (unsigned __int8)sub_4B27090(a1, a2, Src, v21, a5) )// GetFFlag
     {
@@ -50,14 +51,10 @@ const uintptr_t GetFFlag = [FLog::ResetFilters] ParseAndSetFilterValue saving {}
       {
         v36 = "[FLog::ResetFilters] ParseAndSetFilterValue saving {} = {} for {}";
 */
-const uintptr_t SetFFlag = [FLog::FastLogValueChanged] Setting variable {} // func at xref
-const uintptr_t GetTlsPointer = AggregateBroadphaseGrids // return call (click on it and it returns gettlspointer)
-const uintptr_t GetIdentityStruct = GetStyled(): Property %s is write-only // second call in func
-const uintptr_t IdentityPtr = GetStyled(): Property %s is write-only // go into GetIdentityStruct (double click it) and it returns the IdentityPtr to GetTlsPointer
+const uintptr_t SetFFlag = "[FLog::FastLogValueChanged] Setting variable {}" // func at xref
+const uintptr_t GetTlsPointer = "Current identity is %d" // just scroll up a little and a func that looks like sub_xxxx (4 chars) is it (this is the GetTlsPointer_wrapper) but in it u can find the GetTlsPointer (sub_xxxx too)
+const uintptr_t IdentityPtr = "Current identity is %d" // 
 //
-const uintptr_t InstancePush = Binding does not exist // figure it out yourself
-const uintptr_t ClassDescriptor = Could not find property descriptor // figure it out yourslelf
-const uintptr_t GetProperty = Could not find property descriptor // figure it out yourself
 const uintptr_t JobName = HumanoidParallelManagerTaskQueue // 3rd qword in func
 const uintptr_t RawScheduler = HumanoidParallelManagerTaskQueue // dword before xref
 const uintptr_t MaxFps = Out of arbiter nodes: Increase the FInt::TaskSchedulerMaxNumOfArbiters value // figure it out
@@ -65,25 +62,25 @@ const uintptr_t TaskSchedulerPointer = Out of arbiter nodes: Increase the FInt::
 const uintptr_t JobsEnd = Out of arbiter nodes: Increase the FInt::TaskSchedulerMaxNumOfArbiters value // figure it out
 const uintptr_t JobsStart = TaskScheduler // 2nd qword in func i think 
 const uintptr_t ScriptContextResume = "Can't resume script in this context" // func at xref
-const uintptr_t ScriptContextToResume = "Can't resume script in this context" // figure it out
+const uintptr_t ScriptContextToResume = "Can't resume script in this context" // scroll up completely then go a little down till its not random shit anymore, its just a decimal
 //
-const uintptr_t BitMap = 0x7fffffffffff // constant in RobloxPlayerBeta.dll
-const uintptr_t ControlFlowGuard = 0x7fffffffffff // constant in RobloxPlayerBeta.dll
+const uintptr_t BitMap = "0x7fffffffffff" // constant in RobloxPlayerBeta.dll
+const uintptr_t ControlFlowGuard = "0x7fffffffffff" // constant in RobloxPlayerBeta.dll
 
 // raknet (tested on version-5cf2272675e145f5)
 // 3/4 are accurate
-const uintptr_t Send = [DFLog::ReduceMeshDebugLog] ReducedMeshAeroForceModel: original #tris = {} // NOT ACCURATE
-const uintptr_t ProcessNetworkPacket = RakPeer::ProcessNetworkPacket
-const uintptr_t ReportNetworkError = [DFLog::NetworkTrace] reportPerServerMetric::: sc(%s:%d).state = %d :: returning false
-const uintptr_t HandleConnectionState = [DFLog::NetworkTrace] updateServerConnectionState::: sc(%s).old_state = %d -> new_state = %d :: returning false
+const uintptr_t Send = "[DFLog::ReduceMeshDebugLog] ReducedMeshAeroForceModel: original #tris = {}" // not accurate
+const uintptr_t ProcessNetworkPacket = "RakPeer::ProcessNetworkPacket"
+const uintptr_t ReportNetworkError = "[DFLog::NetworkTrace] reportPerServerMetric::: sc(%s:%d).state = %d :: returning false"
+const uintptr_t HandleConnectionState = "[DFLog::NetworkTrace] updateServerConnectionState::: sc(%s).old_state = %d -> new_state = %d :: returning false"
   
 // task lib
-const uintptr_t task.defer = task.defer is not available for AuroraScripts
-const uintptr_t task.spawn = task.spawn is not available for AuroraScripts
-const uintptr_t task.desynchronize = task.desynchronize() may only be called from a script that is a descendant of an Actor
-const uintptr_t task.synchronize = task.synchronize() may only be called from a script that is a descendant of an Actor
-const uintptr_t task.wait = task.wait is not available for AuroraScripts
-const uintptr_t task.cancel = cannot cancel thread
+const uintptr_t task.defer = "task.defer is not available for AuroraScripts"
+const uintptr_t task.spawn = "task.spawn is not available for AuroraScripts"
+const uintptr_t task.desynchronize = "task.desynchronize() may only be called from a script that is a descendant of an Actor"
+const uintptr_t task.synchronize = "task.synchronize() may only be called from a script that is a descendant of an Actor"
+const uintptr_t task.wait = "task.wait is not available for AuroraScripts"
+const uintptr_t task.cancel = "cannot cancel thread"
 
 // task lib (ez version)
 /*
@@ -111,8 +108,8 @@ const uintptr_t task.cancel = cannot cancel thread
  */
 
 // coroutine lib
-const uintptr_t coroutine.close = cannot close %s coroutine
-const uintptr_t coroutine.resume = too many arguments to resume
+const uintptr_t coroutine.close = "cannot close %s coroutine"
+const uintptr_t coroutine.resume = "too many arguments to resume"
 
 // coroutine lib (ez version)
 /*
@@ -139,7 +136,7 @@ const uintptr_t coroutine.resume = too many arguments to resume
  */
 
 // and because for some reason people cant dump reflection types:
-const uintptr_t ReflectionTypes = ProtectedString // use ur brain
+const uintptr_t ReflectionTypes = "ProtectedString" // use ur brain
 // look at ts: (cleaned up a little bit)
 char *__fastcall sub_8FAF80(int a1) {
     switch (a1) {
@@ -252,3 +249,5 @@ char *__fastcall sub_8FAF80(int a1) {
         default: return &byte_61058A5;
     }
 }
+
+// boom ez
